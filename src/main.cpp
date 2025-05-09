46d9e7d2714fbdd110dc0477e90fb6916238e649
Tobias Köhler <29401795+ToKoel@users.noreply.github.com>
2025-05-09 23:53:34 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "gui_handler.hpp"
 
-int main(int, char**)
-{
-    GuiHandler gui;
-    gui.init();
-    gui.start_main_loop();
-    return 0;
+int main(int, char**) {
+  GuiHandler gui;
+  if (!gui.init()) {
+    return -1;
+  }
+  gui.start_main_loop();
+  return 0;
 }
