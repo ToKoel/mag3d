cec4e081490881334107ebed8f99d3e1668c634f
Tobias Köhler <29401795+ToKoel@users.noreply.github.com>
2025-05-11 17:37:21 +0200

    draw colorful cube

--- src/gui_handler.cpp
+++ src/gui_handler.cpp
@@ -4,6 +4,8 @@
 #include <OpenGL/glu.h>
 #include <stdio.h>
 
+#include <array>
+
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl2.h"
 #include "load_shader.hpp"
@@ -161,14 +163,13 @@ void GuiHandler::handle_events(SDL_Event* event) {
   }
 }
 
-GLuint init_triangle() {
-  GLuint VertexArrayID;
-  glGenVertexArrays(1, &VertexArrayID);
-  glBindVertexArray(VertexArrayID);
-
-  static const GLfloat g_vertex_buffer_data[] = {
-      -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-  };
+template <std::size_t N, std::size_t K>
+std::pair<GLuint, GLuint> init_shape(
+    const std::array<GLfloat, N>& vertex_buffer_data,
+    const std::array<GLfloat, K>& color_buffer_data) {
+  GLuint vertex_array_id;
+  glGenVertexArrays(1, &vertex_array_id);
+  glBindVertexArray(vertex_array_id);
 
   // This will identify our vertex buffer
   GLuint vertexbuffer;
@@ -177,13 +178,65 @@ GLuint init_triangle() {
   // The following commands will talk about our 'vertexbuffer' buffer
   glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
   // Give our vertices to OpenGL.
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
-               g_vertex_buffer_data, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * N, vertex_buffer_data.data(),
+               GL_STATIC_DRAW);
 
-  return vertexbuffer;
+  GLuint colorbuffer;
+  glGenBuffers(1, &colorbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * N, color_buffer_data.data(),
+               GL_STATIC_DRAW);
+
+  return std::pair{vertexbuffer, colorbuffer};
 }
 
-void draw_triangle(GLuint vertexbuffer, GLuint programId) {
+std::pair<GLuint, GLuint> init_triangle() {
+  static const std::array<GLfloat, 9U> g_vertex_buffer_data = {
+      -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+  };
+  static const std::array<GLfloat, 27U> g_color_buffer_data = {
+      0.583f, 0.771f, 0.014f, 0.609f, 0.115f, 0.436f, 0.327f, 0.483f, 0.844f,
+      0.822f, 0.569f, 0.201f, 0.435f, 0.602f, 0.223f, 0.310f, 0.747f, 0.185f,
+      0.597f, 0.770f, 0.761f, 0.559f, 0.436f, 0.730f, 0.359f, 0.583f, 0.152f};
+
+  return init_shape(g_vertex_buffer_data, g_color_buffer_data);
+}
+
+std::pair<GLuint, GLuint> init_cube() {
+  static const std::array<GLfloat, 128U> g_vertex_buffer_data = {
+      -1.0f, -1.0f, -1.0f,                       // triangle 1 : begin
+      -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,   // triangle 1 : end
+      1.0f,  1.0f,  -1.0f,                       // triangle 2 : begin
+      -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f,  // triangle 2 : end
+      1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,
+      1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,
+      -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, 1.0f,  -1.0f, 1.0f,
+      -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f,
+      -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f,
+      -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+      1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, -1.0f,
+      1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,
+      1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  -1.0f, 1.0f};
+
+  static const std::array<GLfloat, 384> g_color_buffer_data = {
+      0.583f, 0.771f, 0.014f, 0.609f, 0.115f, 0.436f, 0.327f, 0.483f, 0.844f,
+      0.822f, 0.569f, 0.201f, 0.435f, 0.602f, 0.223f, 0.310f, 0.747f, 0.185f,
+      0.597f, 0.770f, 0.761f, 0.559f, 0.436f, 0.730f, 0.359f, 0.583f, 0.152f,
+      0.483f, 0.596f, 0.789f, 0.559f, 0.861f, 0.639f, 0.195f, 0.548f, 0.859f,
+      0.014f, 0.184f, 0.576f, 0.771f, 0.328f, 0.970f, 0.406f, 0.615f, 0.116f,
+      0.676f, 0.977f, 0.133f, 0.971f, 0.572f, 0.833f, 0.140f, 0.616f, 0.489f,
+      0.997f, 0.513f, 0.064f, 0.945f, 0.719f, 0.592f, 0.543f, 0.021f, 0.978f,
+      0.279f, 0.317f, 0.505f, 0.167f, 0.620f, 0.077f, 0.347f, 0.857f, 0.137f,
+      0.055f, 0.953f, 0.042f, 0.714f, 0.505f, 0.345f, 0.783f, 0.290f, 0.734f,
+      0.722f, 0.645f, 0.174f, 0.302f, 0.455f, 0.848f, 0.225f, 0.587f, 0.040f,
+      0.517f, 0.713f, 0.338f, 0.053f, 0.959f, 0.120f, 0.393f, 0.621f, 0.362f,
+      0.673f, 0.211f, 0.457f, 0.820f, 0.883f, 0.371f, 0.982f, 0.099f, 0.879f};
+
+  return init_shape(g_vertex_buffer_data, g_color_buffer_data);
+}
+
+void draw_shape(GLuint vertexbuffer, GLuint colorbuffer, GLuint programId,
+                size_t number_of_triangles) {
   glClear(GL_COLOR_BUFFER_BIT);
   glUseProgram(programId);
 
@@ -198,9 +251,24 @@ void draw_triangle(GLuint vertexbuffer, GLuint programId) {
                         0,         // stride
                         (void*)0   // array buffer offset
   );
-  // Draw the triangle !
-  glDrawArrays(GL_TRIANGLES, 0,
-               3);  // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+  // 2nd attribute buffer : colors
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+  glVertexAttribPointer(1,  // attribute. No particular reason for 1, but must
+                            // match the layout in the shader.
+                        3,  // size
+                        GL_FLOAT,  // type
+                        GL_FALSE,  // normalized?
+                        0,         // stride
+                        (void*)0   // array buffer offset
+  );
+
+  // Draw the triangles !
+  glDrawArrays(
+      GL_TRIANGLES, 0,
+      number_of_triangles *
+          3);  // Starting from vertex 0; 3 vertices total -> 1 triangle
   glDisableVertexAttribArray(0);
 }
 
@@ -209,7 +277,7 @@ void GuiHandler::start_main_loop() {
   float angle_x = 0.0f, angle_y = 0.0f;
   bool rotate = true;
 
-  GLuint vertexbuffer = init_triangle();
+  std::pair<GLuint, GLuint> buffer_ids = init_cube();
   GLuint program_id = load_shaders(
       "/Users/tobiaskohler/Documents/projects/magnetic/src/shaders/"
       "triangle.vert",
@@ -236,7 +304,7 @@ void GuiHandler::start_main_loop() {
 
     draw_control_window(&angle_x, &angle_y);
 
-    draw_triangle(vertexbuffer, program_id);
+    draw_shape(buffer_ids.first, buffer_ids.second, program_id, 12);
     glUniformMatrix4fv(matrix_id, 1, GL_FALSE, &mvp[0][0]);
 
     ImGui::Render();
